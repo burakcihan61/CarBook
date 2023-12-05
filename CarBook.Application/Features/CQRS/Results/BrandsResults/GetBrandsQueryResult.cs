e8316f81d1658f89dbef0ed462a6057d156db694
Burak Cihan <burakcihan622@gmail.com>
2023-12-05 14:06:28 +0300

    Update GetBrandsQueryResult.cs

--- CarBook.Application/Features/CQRS/Results/BrandsResults/GetBrandsQueryResult.cs
+++ CarBook.Application/Features/CQRS/Results/BrandsResults/GetBrandsQueryResult.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CarBook.Application.Features.CQRS.Results.BrandsResults
+﻿namespace CarBook.Application.Features.CQRS.Results.BrandsResults
 {
-    public class GetBrandsQueryResult { }
+    public class GetBrandsQueryResult
+    {
+        public int BrandID { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
 }
