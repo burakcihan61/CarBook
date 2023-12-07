df9eff1eda101942d7447e8a23cd8173260501bc
Burak Cihan <burakcihan622@gmail.com>
2023-12-07 15:36:24 +0300

    Update GetContactQueryResult.cs

--- CarBook.Application/Features/CQRS/Results/ContactResults/GetContactQueryResult.cs
+++ CarBook.Application/Features/CQRS/Results/ContactResults/GetContactQueryResult.cs
@@ -6,5 +6,13 @@ using System.Threading.Tasks;
 
 namespace CarBook.Application.Features.CQRS.Results.ContactResults
 {
-    public class GetContactQueryResult { }
+    public class GetContactQueryResult
+    {
+        public int ContactID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public DateTime SendDate { get; set; } = DateTime.Now;
+    }
 }
